ed8b79010d1dbca94442e42261070cb8f2dda850
Mehdi Chou. <mehdichou69@gmail.com>
2018-12-21 15:43:57 +0100

    update commit

--- Jdk8News/src/mc/tuto/main/Main.java
+++ Jdk8News/src/mc/tuto/main/Main.java
@@ -5,7 +5,9 @@ import mc.tuto.jdk8news.Jdk8Tests;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("*************************************************");
 		Jdk8Tests.foreachTests();
+		System.out.println("*************************************************");
 	}
 
 }
